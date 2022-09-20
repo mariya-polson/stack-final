f8b94f15def44d22317e16c1ace4a2435652c53d
mariya-polson <108417067+mariya-polson@users.noreply.github.com>
2022-09-20 16:36:08 +0530

    Add files via upload

---
+++ stack-wepage/files/script.js
@@ -0,0 +1,10 @@
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
